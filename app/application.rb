6329ec72b087125f52f6171937d62b20d4c880f0
Michael Evans <michael@michaelgregoryevans.com>
2017-12-04 22:25:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry
+    #binding.pry
     resource = "/#{@@items.detect{|i| i.name == req.path.split("/").last}}"
 
     if req.path == resource
