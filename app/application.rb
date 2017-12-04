14c7d1f58a388fe7ef5e652abca6d88ca25b0299
Michael Evans <michael@michaelgregoryevans.com>
2017-12-04 22:38:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    resource = "items/#{@@items.detect{|i| i.name == req.path.split("/").last}.name}" if @@items != []
+    resource = "/items/#{@@items.detect{|i| i.name == req.path.split("/").last}.name}" if @@items != []
     binding.pry
     if req.path == resource
 
