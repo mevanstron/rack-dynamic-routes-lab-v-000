35d61bde6bde90d6ba7215517251f02f7647ed07
Michael Evans <michael@michaelgregoryevans.com>
2017-12-04 22:45:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,11 @@ class Application
     item = @@items.detect{|i| i.name == req.path.split("/").last}
     resource = "/items/#{item.name}" if @@items != []
     #binding.pry
-    if req.path == resource
+    if req.path.match(/items/)
       resp.write "#{item.price}"
-      200
+    elsif condition
+      resp.write "Item not found"
+      resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 404
